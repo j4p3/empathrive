9c10bbf9d369c5a033ed8d610298546dee8ea224
JP <bonnerjp@gmail.com>
2013-01-29 17:15:33 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Empathrive::Application.config.secret_token = '359bdd21e21ff682c011e32210fca5728f99c73c4148128b432286e0f4d7295596f68db55c7f601fdf143f89a7c668037cc665321a5273515513fb8fa6653682'
---
+++ readme.md
@@ -0,0 +1,3 @@
+###Empathrive
+
+*when the game show contestent gets hit in the balls with the big padded paddle, we wince in empathy. when the self-empowered heroine is betrayed by the hunky love interest, we consume an empathetic tub of Ben & Jerry's. isn't it time somebody made some money off this shit?*
