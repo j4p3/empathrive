e9ad2440b10e8c92d26fae384c4193b46847f7b8
JP <bonnerjp@gmail.com>
2013-03-30 13:28:54 +0100

    changed asset compile settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,7 @@ Empathrive::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
@@ -15,7 +16,8 @@ Empathrive::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
